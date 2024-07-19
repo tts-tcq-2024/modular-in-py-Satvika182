69953ec9e6bcdab282df0e6461fbe0a5669f6cc8
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-19 12:39:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
-
 def color_pair_to_string(major_color, minor_color):
   return f'{major_color} {minor_color}'
-
 
 def get_color_from_pair_number(pair_number):
   zero_based_pair_number = pair_number - 1
@@ -16,7 +13,6 @@ def get_color_from_pair_number(pair_number):
   if minor_index >= len(MINOR_COLORS):
     raise Exception('Minor index out of range')
   return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
-
 
 def get_pair_number_from_color(major_color, minor_color):
   try:
@@ -30,22 +26,3 @@ def get_pair_number_from_color(major_color, minor_color):
   return major_index * len(MINOR_COLORS) + minor_index + 1
 
 
-def test_number_to_pair(pair_number,
-                        expected_major_color, expected_minor_color):
-  major_color, minor_color = get_color_from_pair_number(pair_number)
-  assert(major_color == expected_major_color)
-  assert(minor_color == expected_minor_color)
-
-
-def test_pair_to_number(major_color, minor_color, expected_pair_number):
-  pair_number = get_pair_number_from_color(major_color, minor_color)
-  assert(pair_number == expected_pair_number)
-
-
-if __name__ == '__main__':
-  test_number_to_pair(4, 'White', 'Brown')
-  test_number_to_pair(5, 'White', 'Slate')
-  test_pair_to_number('Black', 'Orange', 12)
-  test_pair_to_number('Violet', 'Slate', 25)
-  test_pair_to_number('Red', 'Orange', 7)
-  print('Done :)')
