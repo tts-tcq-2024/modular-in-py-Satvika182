2099bbf9e96152d4e94050102cde896803d1a89b
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-19 12:33:51 +0530

    Update main test.py

--- main test.py
+++ main test.py
@@ -8,3 +8,12 @@ def test_number_to_pair(pair_number,
 def test_pair_to_number(major_color, minor_color, expected_pair_number):
   pair_number = get_pair_number_from_color(major_color, minor_color)
   assert(pair_number == expected_pair_number)
+
+
+if __name__ == '__main__':
+  test_number_to_pair(4, 'White', 'Brown')
+  test_number_to_pair(5, 'White', 'Slate')
+  test_pair_to_number('Black', 'Orange', 12)
+  test_pair_to_number('Violet', 'Slate', 25)
+  test_pair_to_number('Red', 'Orange', 7)
+  print('Done :)')
