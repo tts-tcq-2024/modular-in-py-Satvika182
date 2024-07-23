caae1e8213ed20ec5f93c4feef36ceab5dc41b7c
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-23 12:20:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from color_mapping import generate_color_code_reference
+from test_color_mapping import tests
+from generate_reference_manual import generate_color_code_reference
+
+def main():
+     tests()
+     create_reference_manual()
+
 
 if __name__ == '__main__':
-    reference = generate_color_code_reference()
-    print('Color Code Reference Manual:')
-    for line in reference:
-        print(line)
+    main()
