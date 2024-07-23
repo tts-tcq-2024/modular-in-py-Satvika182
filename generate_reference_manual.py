cc76d3dfc483df271e8e9e2e88956256608104c0
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-23 10:35:07 +0530

    Update generate_reference_manual.py

--- generate_reference_manual.py
+++ generate_reference_manual.py
@@ -1,5 +1,5 @@
-from color_mapping import print_color_code_reference
+from color_mapping import create_color_code_reference
 
 if __name__ == '__main__':
-    print('Color Code Reference Manual:')
-    print(print_color_code_reference())
+    reference = create_color_code_reference()
+   
