ab07b9a6fe550758cef37ab765c33e7a9b51dbbd
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-23 11:21:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
+from color_mapping import generate_color_code_reference
 
-def color_pair_to_string(major, minor):
-    return f'{major} {minor}'
-
-def get_color_from_pair_number(pair_number):
-    zero_based_pair_number = pair_number - 1
-    major_index = zero_based_pair_number // len(MINOR_COLORS)
-    if major_index >= len(MAJOR_COLORS):
-        raise Exception('Major index out of range')
-    minor_index = zero_based_pair_number % len(MINOR_COLORS)
-    return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
-
-def get_pair_number_from_color(major, minor):
-    if major not in MAJOR_COLORS:
-        raise Exception('Major index out of range')
-    if minor not in MINOR_COLORS:
-        raise Exception('Minor index out of range')
-    return MAJOR_COLORS.index(major) * len(MINOR_COLORS) + MINOR_COLORS.index(minor) + 1
+if __name__ == '__main__':
+    reference = generate_color_code_reference()
+    print('Color Code Reference Manual:')
+    for line in reference:
+        print(line)
