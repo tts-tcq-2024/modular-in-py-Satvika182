c20546fc49538d0a7dee0bb77bab57c90afb3a44
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-23 12:23:11 +0530

    Update generate_reference_manual.py

--- generate_reference_manual.py
+++ generate_reference_manual.py
@@ -2,8 +2,7 @@ from color_mapping import MAJOR_COLORS, MINOR_COLORS, get_pair_number_from_color
 
 def generate_color_code_reference():
     reference =[]
-    return [f'{pair}: {color_pair_to_string(*get_color_from_pair_number(pair))}' 
-            for pair in range(1, 26)]
+    return [f'{pair}: {color_pair_to_string(*get_color_from_pair_number(pair))}' for pair in range(1, 26)]
 
 
 if __name__ == '__main__':
